82bdee0acdac05799ed67f6b162686ea0d3d3d4e
Mark Bixler <mbixler@solutionsitw.com>
2018-08-14 13:30:44 -0400

    Added specific error codes. Also added error logging for CodeDeploy.

--- scripts/run-upgrade.bat
+++ scripts/run-upgrade.bat
@@ -1,3 +1,23 @@
-start /wait "Solutions POS Upgrader" C:\temp\upgrader.exe /S
+@echo off
 
-exit %ERRORLEVEL%
+:: Define Error Constants
+SET /A POS_NOT_INSTALLED=22
+SET /A POS_RUNNING=23
+
+:: Run Upgrader
+start /wait "Solutions POS Upgrader" C:\temp\upgrader.exe /S
+set returnCode=%ERRORLEVEL%
+
+:: If there is an error, Print the type and exit with corresponding error code.
+if /I "%returnCode%" EQU "%POS_NOT_INSTALLED%" (
+    echo Unable to update Solutions POS. Application not installed.
+    exit %POS_NOT_INSTALLED%
+)
+
+if /I "%returnCode%" EQU "%POS_RUNNING%" (
+    echo Unable to update Solutions POS. Application running.
+    exit %POS_RUNNING%
+)
+
+:: No errors. Return Success.
+exit 0
