c3cc96a76ba04233421f25db766d28a95b537008
Mark Bixler <mbixler@solutionsitw.com>
2018-08-15 15:32:22 -0400

    added backup before update runs

--- scripts/before-install.bat
+++ scripts/before-install.bat
@@ -1,1 +1,4 @@
-echo "Finished" > before.txt
+if not exist "C:\ProgramData\Solutions POS\" (
+    mkdir "C:\ProgramData\Solutions POS"
+)
+start /wait "POS Backup" robocopy "C:\Program Files (x86)\Solutions POS" "C:\ProgramData\Solutions POS\Backup" /E
--- scripts/run-upgrade.bat
+++ scripts/run-upgrade.bat
@@ -3,6 +3,9 @@
 :: Define Error Constants
 SET /A POS_NOT_INSTALLED=22
 SET /A POS_RUNNING=23
+SET /A SQL_UPDATE_FAILED=24
+
+SET ROLLBACK=False
 
 :: Run Upgrader
 start /wait "Solutions POS Upgrader" C:\temp\upgrader.exe /S
@@ -17,6 +20,16 @@ if /I "%returnCode%" EQU "%POS_NOT_INSTALLED%" (
 if /I "%returnCode%" EQU "%POS_RUNNING%" (
     echo Unable to update Solutions POS. Application running.
     exit %POS_RUNNING%
+)
+
+if /I "%returnCode%" EQU "%SQL_UPDATE_FAILED%" (
+    echo Unable to update Solutions POS. SQL Update Failed.
+    set ROLLBACK=True
+)
+
+if /I "%ROLLBACK%" EQU "True" (
+    rmdir /Q /S "C:\Program Files (x86)\Solutions POS"
+    start /wait "POS Rollback" robocopy "C:\ProgramData\Solutions POS\Backup" "C:\Program Files (x86)\Solutions POS"
 )
 
 :: No errors. Return Success.
